79fd2a4a77fad6f0de6110332b2cc2f35f1487b0
Junegithub123 <junejose123@gmail.com>
2023-03-15 17:12:07 +0530

    March-15-2023 Dasboard Orders, Employees pages Updates

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, ContextMenu, Filter, Sort, Page, Search, Inject, Edit, Toolbar } from '@syncfusion/ej2-react-grids';
+import { employeesGrid, employeesData, contextMenuItems } from '../data/dummy';
+import { Header } from '../components';
 
-function Employees() {
+const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width="auto"
+        //allowExcelExport
+        //allowPdfExport
+        //contextMenuItems={contextMenuItems}
+        //editSettings={editing}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[ Page, Search, Toolbar ]} />
+      </GridComponent>
+    </div>
   )
-}
+};
 
 export default Employees
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, ContextMenu, Filter, Sort, Page, ExecelExport, PdfExport, Inject, Edit } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, ContextMenu, Filter, Sort, Page, ExcelExport, PdfExport, Inject, Edit } from '@syncfusion/ej2-react-grids';
 import { ordersGrid, ordersData, contextMenuItems } from '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        //allowExcelExport
+        //allowPdfExport
+        //contextMenuItems={contextMenuItems}
+        //editSettings={editing}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {ordersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+      </GridComponent>
+    </div>
   )
 };
 
